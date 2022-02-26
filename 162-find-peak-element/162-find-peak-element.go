4e32f224302b1769d3fb0f849f7916585c0a6c0c
Siddharth Srinivasan <13200018+siddharths2710@users.noreply.github.com>
2022-02-26 13:09:18 -0700

    Time: 6 ms (39.15%), Space: 2.8 MB (84.12%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.go
@@ -0,0 +1,13 @@
+func findPeakElement(nums []int) int {
+    l := 0
+    r := len(nums) - 1
+    for l < r {
+        mid := ( l + r ) / 2
+        if nums[mid] > nums[mid + 1] {
+            r = mid
+        } else {
+            l = mid + 1
+        }
+    }
+    return l
+}
